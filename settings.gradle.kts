8e3fc5cb542233e8654aa0a7072738683214e251
MrPREDDY18 <144317406+MrPREDDY18@users.noreply.github.com>
2025-06-26 21:22:17 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io") // ✅ Correct Kotlin syntax
+    }
+}
+
+rootProject.name = "OPSC_P2_AMEER_MILLER_ST10067171"
+include(":app")
